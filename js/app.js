090b1d28ff05d4edc5ad98d7baba1230d2257b0c
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-01-09 20:38:07 +0600

    added location API and displayed

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
-var x = document.getElementById("show_cords"); 
-var show = document.getElementById("show_res"); 
+var x = document.getElementById("show_cords");
+var show = document.getElementById("show_res");
 var lat = "";
 var long = "";
 const apikey = "e6f24338b0994586b33d707fa681008b";
 
+// GETTING BROWSER GPS LOCATION
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -15,23 +16,48 @@ function getLocation() {
 function showPosition(position) {
   lat = position.coords.latitude;
   long = position.coords.longitude;
-  //   x.value = position.coords.latitude+","+position.coords.longitude;
 
+  //showing data to input field
   x.value = lat + "," + long;
+  getCity(lat, long, apikey);
+}
 
+window.onload = function () {
+  getLocation();
+};
+
+
+
+function getCity(lat, long, apikey) {
+  // debug 
+  console.log("Lat: " + lat + " Long: " + long + " Api: " + apikey);
+
+  // Api methods starts
   var requestOptions = {
     method: "GET",
   };
-  var responseArray=0;
-  fetch("https://api.geoapify.com/v1/geocode/reverse?lat="+lat+"&lon="+long+"&format=json&type=city&apiKey="+apikey+"",requestOptions)
+  fetch("https://api.geoapify.com/v1/geocode/reverse?lat="+lat+"&lon="+long +"&format=json&type=city&apiKey="+apikey, requestOptions)
     .then((response) => response.json())
-    .then((result) => console.log(result))
-    // .then((result) => responseArray)
-    // .then((result) =>   show.innerHTML(result))
+    .then((result) => assign(result))
     .catch((error) => console.log("error", error));
-
-
-    // console.log(results.city);
 }
 
+function assign(data) {
+  gps = data.results[0];
+  placename = gps.name;
+  city = gps.city;
+  district = gps.county;
+  country = gps.country;
+  division = gps.state;
+  postcode = gps.postcode;
 
+// --html -- 
+document.getElementById("placename").innerHTML=placename;
+document.getElementById("city").innerHTML=city;
+document.getElementById("district").innerHTML=district;
+document.getElementById("country").innerHTML=country;
+document.getElementById("division").innerHTML=division;
+document.getElementById("postcode").innerHTML=postcode;
+  // console.log(gpsCity);
+  console.log(gps);
+}
